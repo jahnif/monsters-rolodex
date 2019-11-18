cb844d8466e4768011e743490f432ddad9f7093b
jahnif <joseph.peha@gmail.com>
2019-11-17 23:01:34 -0800

    Updates

---
+++ precache-manifest.c3915ffcd38320aaa33f9efdd8488885.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c57d1ccce3c8a940008687b919fade8f",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "5d2b6947290814660bbb",
+    "url": "/monsters-rolodex/static/css/main.c94720b1.chunk.css"
+  },
+  {
+    "revision": "b52a17cd64a882b9e1b8",
+    "url": "/monsters-rolodex/static/js/2.6a44ca9a.chunk.js"
+  },
+  {
+    "revision": "5d2b6947290814660bbb",
+    "url": "/monsters-rolodex/static/js/main.f116bf4e.chunk.js"
+  },
+  {
+    "revision": "792a51b6ecba803ed03e",
+    "url": "/monsters-rolodex/static/js/runtime-main.f88ecf1a.js"
+  }
+]);
