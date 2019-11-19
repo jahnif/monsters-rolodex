815298ec70764d616e1e4fb8a5952265e564174d
jahnif <joseph.peha@gmail.com>
2019-11-18 22:18:39 -0800

    Updates

--- precache-manifest.b22220faf4b0b9335497c56dda7c34c3.js
+++ precache-manifest.b22220faf4b0b9335497c56dda7c34c3.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "c57d1ccce3c8a940008687b919fade8f",
+    "revision": "3b8adebab99abe8a5f26e155b9cbcd66",
     "url": "/monsters-rolodex/index.html"
   },
   {
-    "revision": "5d2b6947290814660bbb",
+    "revision": "1c17d0a16f10c3f16570",
     "url": "/monsters-rolodex/static/css/main.c94720b1.chunk.css"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/monsters-rolodex/static/js/2.6a44ca9a.chunk.js"
   },
   {
-    "revision": "5d2b6947290814660bbb",
-    "url": "/monsters-rolodex/static/js/main.f116bf4e.chunk.js"
+    "revision": "1c17d0a16f10c3f16570",
+    "url": "/monsters-rolodex/static/js/main.d30ec41c.chunk.js"
   },
   {
     "revision": "792a51b6ecba803ed03e",
